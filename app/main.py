885b345a8ace2ea755edb30117e4ac01b202d4e8
el hidari nouhayla <elhidarinouhayla@gmail.com>
2026-01-15 11:59:01 +0100

    creation d'endpoint de creation d'un user

--- app/main.py
+++ app/main.py
@@ -1,8 +1,39 @@
-import pandas as pd
+from fastapi import FastAPI, HTTPException,Depends
+from app.database import Base, engine, get_db
+from sqlalchemy.orm import session
+from app.models.schema import UserCreate, UserResponse, UserVerify
+from app.models.model import User
+from .auth import create_token, verify_password, verify_token, hache_password
 
-# Lecture du fichier
-df = pd.read_parquet('data/bronze_taxi.parquet')
 
-# Vérification
-print(df.head())
-print(df.info())
+
+
+app = FastAPI()
+Base.metadata.create_all(bin=engine)
+
+
+
+# creation d'un username :
+@app.post("/register", response_model=UserResponse)
+def create_user(user:UserCreate, db: session=Depends(get_db)):
+    exist = db.query(User).filter(User.username == user.username).first()
+
+    if exist:
+        raise HTTPException(status_code=400, detail= "username existe deja")
+    
+    # haching password
+    hashed_pwd = hache_password(user.password)
+    
+    new_user = User(username=user.username, password=hashed_pwd, email=user.email)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+
+
+
+
+
+
+
