e7d44b686f72e8adb549253959f6073f54131f83
el hidari nouhayla <elhidarinouhayla@gmail.com>
2026-01-15 11:57:39 +0100

    add config file

--- app/config.py
+++ app/config.py
@@ -4,8 +4,13 @@ import os
 load_dotenv()
 
 
-USER=os.getenv("USER", "DB_User")
-PASSWORD=os.getenv("PASSWORD", "DB_PASSWORD")
-PORT=os.getenv("PORT",5432)
-HOST=os.getenv("HOST", "db")
-DATABASE=os.getenv("DATABASE", "DB_NAME")
+USER=os.getenv("USER")
+PASSWORD=os.getenv("PASSWORD")
+PORT=os.getenv("PORT")
+HOST=os.getenv("HOST")
+DATABASE=os.getenv("DATABASE")
+
+
+
+SECRET_KEY=os.getenv("SECRET_KEY")
+ALGORITHM=os.getenv("ALGORITHM")
