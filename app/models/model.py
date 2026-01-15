f2ce8ddb3a90ad835569e72c78a1f7994c5ed281
el hidari nouhayla <elhidarinouhayla@gmail.com>
2026-01-15 11:58:08 +0100

    creation du BaseModel

---
+++ app/models/model.py
@@ -0,0 +1,15 @@
+from app.database import Base
+from sqlalchemy import String, Integer, Column, DateTime
+from datetime import datetime
+
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    password = Column(Integer, nullable=False)
+    create_at = Column(DateTime, default=datetime.now)
+
